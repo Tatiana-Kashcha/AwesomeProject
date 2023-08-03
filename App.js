a6e1fe74382f0b016be11853e52034b89b0caaac
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-03 18:12:30 +0300

    add project

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  TextInput, View, Text, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.header}>Увійти</Text>
+      <TextInput style={styles.input} placeholder='Адреса електронної пошти'/>
+      <TextInput style={styles.input} />
+      <Button style="auto" title='Увійти'/>
+      <Text style="auto">Немає акаунту? Зареєструватися</Text>
+       <StatusBar style="auto"/>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.6,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  header: {
+      fontFamily: 'Roboto',
+      fontSize: 30,
+      fontWeight: 'medium',
+  },
+  input: {
+    borderColor: '#E8E8E8',
+    backgroundColor: '#f6f6f6',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
---
+++ src/components/LoginScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View style={}>
+<Text style={}>Увійти</Text>
+<TextInput style={} placeholder='Адреса електронної пошти'/>
+<TextInput style={} />
+<Button style={} title='Увійти'/>
+<Text style={}>Немає акаунту? Зареєструватися</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
