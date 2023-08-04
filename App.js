c396cacf79a140f1d4051bde40c376145c552d63
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-04 15:08:35 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  TextInput, View, Text, Button } from 'react-native';
+import { StyleSheet,  TextInput, View, SafeAreaView, Text, Button, Image } from 'react-native';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto"/>
       <View style={styles.thumb}>
-
         <Text style={styles.header}>Увійти</Text>
-        <TextInput style={styles.input} placeholder='Адреса електронної пошти'/>
-        <TextInput style={styles.input} />
-        <Button style={styles.button} title='Увійти'/>
-        <Text style="auto">Немає акаунту? Зареєструватися</Text>
-        
-        <StatusBar style="auto"/>
 
+        <View style={styles.form}>
+        <TextInput style={styles.input} placeholder='Адреса електронної пошти'/>
+        <TextInput style={styles.input} placeholder='************'/>
+        <Button padding= '10' borderRadius= '100' color="#FF6C00" title='Увійти'  />
+        </View>
+        <Text style={styles.textDesc}>
+          Немає акаунту? 
+          <Text style={styles.textReg}>Зареєструватися</Text>
+        </Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { 
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'flex-end',
@@ -32,26 +34,43 @@ const styles = StyleSheet.create({
     flex: 0.6,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
     borderWidth: 1,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
+    paddingLeft: 16,
+    paddingRight: 16,
+    
   },
   header: {
       fontFamily: 'Roboto',
       fontSize: 30,
       fontWeight: 'medium',
+      marginTop: 32,
+  },
+  form: {
+    width: '100%',
+    marginTop: 32,
   },
   input: {
     borderColor: '#E8E8E8',
     backgroundColor: '#f6f6f6',
-    height: 40,
-    margin: 12,
+    width: '100%',
     borderWidth: 1,
-    padding: 10,
+    borderRadius: 8,
+    padding: 16,
+    
   },
   button: {
-    color: '#f6f6f6',
     padding: 10,
+    borderRadius: 100,
+    color:"#FF6C00",
   },
+  textDesc: {
+    color: '#1B4371',
+    marginTop: 16,
+  },
+  textReg: {
+    textDecorationLine: 'underline',
+    marginLeft: 4,
+  }
 });
