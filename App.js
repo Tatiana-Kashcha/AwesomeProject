974fdb2a730d711ee27720c6cc748bf817647d54
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-05 00:32:25 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView } from 'react-native';
+import { useFonts } from 'expo-font';
 import LoginScreen from './src/components/LoginScreen';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Roboto-Medium': require('./src/assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto"/>
