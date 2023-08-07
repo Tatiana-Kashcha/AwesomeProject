fac046076f107f23a68ed232258ac450dc86d961
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-07 11:24:16 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView } from 'react-native';
-import { useFonts } from 'expo-font';
-import LoginScreen from './src/components/LoginScreen';
-import { gStyle } from './styles/gStyle';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, ImageBackground } from "react-native";
+import { useFonts } from "expo-font";
+import LoginScreen from "./src/components/LoginScreen";
+import { gStyle } from "./styles/gStyle";
+
+const background = { uri: "https://legacy.reactjs.org/logo-og.png" };
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
-    'Roboto-Medium': require('./src/assets/fonts/Roboto-Medium.ttf'),
-    'Roboto-Regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
+    "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -17,12 +19,10 @@ export default function App() {
 
   return (
     <SafeAreaView style={gStyle.container}>
-      <StatusBar style="auto"/>
-      <LoginScreen/>
+      <StatusBar style="auto" />
+      <LoginScreen />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
